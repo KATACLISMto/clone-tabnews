2fcfb6a46e1524e5bad6439978da75e296e026a7
KATACLISMto <karloskatus@gmail.com>
2024-07-25 16:32:44 +0000

    loucura loucura loucura

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+function Home(){
+ return(
+    <div>
+        <h1>amor i love you</h1>
+        <a href="https://www.youtube.com/watch?v=PWy4LyyZk4s&ab_channel=macgyvernews">clica ai meu amor</a>
+            
+            </div>
+ )
+}
+ export default Home
