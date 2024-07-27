45ebfe7cee6ee978217526eb99dafcafe7492da7
KATACLISMto <karloskatus@gmail.com>
2024-07-27 21:15:19 +0000

    modificaçoes

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
-function Home(){
- return(
-    <div>
-        <h1>amor i love you</h1>
-        <p>i love you to</p>
-            
-            </div>
- )
- 
+function Home() {
+  return (
+  <div>
+  <h1>amor i love you</h1>
+  <p>i love you too</p>
+  </div>
+  );
 }
- export default Home
+export default Home;
+
+
+
